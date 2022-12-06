f265d5746b65f5d4ea98a019712358957353dfe2
omegabunh <omegabun@gmail.com>
2022-12-06 16:13:22 +0900

    list view

--- SwiftUIMemo/SwiftUIMemoApp.swift
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoApp: App {
+    @StateObject var store = MemoStore()
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext).environmentObject(store)
         }
     }
 }
